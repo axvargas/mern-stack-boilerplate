a5b459e53f254afd77716a73a4d453116fcfb0b9
axvargas <axvargas@fiec.espol.edu.ec>
2020-05-02 01:44:46 -0500

    make it better 2

--- backend/database.js
+++ backend/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const connection = process.env.MONGODB_URI || 'mongodb://localhost/stack-mern-db';
+const connection = 'mongodb+srv://axvargas:ale1608@cluster0-4pk1v.mongodb.net/test?retryWrites=true&w=majority';
 console.log(connection);
 mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
     .then(() => console.log("Database Connected Successfully"))
